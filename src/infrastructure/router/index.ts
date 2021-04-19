66ec686742f907a89d76f180923807140147f1f6
apple-yagi <applesryu@gmail.com>
2021-04-19 20:50:29 +0900

    add ts-node-dev

--- src/infrastructure/router/index.ts
+++ src/infrastructure/router/index.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import { usersRouter } from "./user.router";
 const router = Router();
 
+router.get("/", function (req, res) {
+  res.send("Hello World");
+});
 router.use("/users", usersRouter);
 
 export { router };
