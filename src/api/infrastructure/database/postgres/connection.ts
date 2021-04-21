9af28933b8aaf32c17feb069a7d9b61c5ff2886c
ryuya-yanagi <79124542+ryuya-yanagi@users.noreply.github.com>
2021-04-21 13:31:25 +0900

    update

--- src/api/infrastructure/database/postgres/connection.ts
+++ src/api/infrastructure/database/postgres/connection.ts
@@ -3,18 +3,23 @@ import { Connection, createConnection } from "typeorm";
 const isDev = process.env.NODE_ENV === "development";
 
 export const connection = (): Promise<Connection> => {
-	return createConnection({
-		type: "postgres",
-		host: process.env.DB_HOST || "localhost",
-		port: parseInt(process.env.DB_PORT) || 5432,
-		username: process.env.DB_USERNAME || "postgres",
-		password: process.env.DB_PASSWORD || "password",
-		database: process.env.DB_NAME || "postgres",
-		entities: [
-			process.cwd() +
-				"/src/api/infrastructure/database/postgres/entity/*.entity.ts"
-		],
-		synchronize: isDev,
-		logging: isDev ? "all" : ["error"]
-	});
+	try {
+		return createConnection({
+			type: "postgres",
+			host: process.env.POSTGRES_HOST || "localhost",
+			port: parseInt(process.env.POSTGRES_PORT) || 5432,
+			username: process.env.POSTGRES_USER || "postgres",
+			password: process.env.POSTGRES_PASSWORD || "password",
+			database: process.env.POSTGRES_DB || "postgres",
+			entities: [
+				process.cwd() +
+					"/src/api/infrastructure/database/postgres/entity/*.entity.ts"
+			],
+			synchronize: isDev,
+			logging: isDev ? "all" : ["error"]
+		});
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
 };
